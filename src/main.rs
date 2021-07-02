d4ed879884909c550adc55f57522888ae829f77c
Chris Konstad <chriskon149@gmail.com>
2021-07-01 21:36:11 -0700

    Got heatmap working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
-use console::style;
+use console::{Style, style};
 use hdrhistogram::Histogram;
 use regex::Regex;
+use std::convert::From;
 use std::io::{self, BufRead};
 use structopt::StructOpt;
 
@@ -9,15 +10,34 @@ use structopt::StructOpt;
 // TODO(ckonstad)
 //  -filter
 //  -sort
-//  -heatmap
 
 #[derive(Debug)]
 enum Data {
     Matching {
         line: String,
         range: std::ops::Range<usize>,
+        parsed: u64,
     },
     NotMatching(String),
+}
+
+#[derive(Debug)]
+enum Percentile {
+    P99,
+    P90,
+    P50,
+    Other,
+}
+
+impl From<Percentile> for Style {
+    fn from(p: Percentile) -> Self {
+        match p {
+            Percentile::P99 => Style::new().red(),
+            Percentile::P90 => Style::new().yellow(),
+            Percentile::P50 => Style::new().green(),
+            Percentile::Other => Style::new().blue(),
+        }
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -25,21 +45,32 @@ struct Opt {
     /// The regex expression used to parse the logs
     expr: String,
 
-    /// If we should highlight data used
+    /// If we should highlight data used.  This disables the heatmap.
     #[structopt(long)]
     highlight: bool,
 
     /// If we should make the text bold
     #[structopt(long, short)]
     bold: bool,
+
+    /// If we should print some debug stats at the end
+    #[structopt(long)]
+    debug: bool,
+
+    /// If we should force color output
+    #[structopt(short, long)]
+    force_colors: bool,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     let re = Regex::new(&opt.expr)?;
 
-    let stdin = io::stdin();
+    if opt.force_colors {
+        console::set_colors_enabled(opt.force_colors);
+    }
 
+    let stdin = io::stdin();
     let mut hist = Histogram::<u64>::new(5)?;
 
     let data = stdin.lock().lines().map(|line| {
@@ -48,26 +79,47 @@ fn main() -> Result<()> {
             if let Some(m) = captures.get(1) {
                 if let Ok(f) = line[m.start()..m.end()].parse::<u64>() {
                     hist += f;
+
+                    return Data::Matching {
+                        range: m.range(),
+                        line,
+                        parsed: f,
+                    };
                 }
-                return Data::Matching {
-                    range: m.range(),
-                    line,
-                };
             }
         }
         Data::NotMatching(line)
     })
     .collect::<Vec<_>>();
 
+    let p99 = hist.value_at_quantile(0.99);
+    let p90 = hist.value_at_quantile(0.90);
+    let p50 = hist.value_at_quantile(0.50);
+
+    let to_percentile = |val| {
+        if p99 <= val {
+            Percentile::P99
+        } else if p90 <= val {
+            Percentile::P90
+        } else if p50 <= val {
+            Percentile::P50
+        } else {
+            Percentile::Other
+        }
+    };
+
     data.into_iter().for_each(|data| {
         match data {
             Data::NotMatching(line) => println!("{}", line),
-            Data::Matching {line, range} => {
+            Data::Matching {line, range, parsed} => {
                 let before = &line[0..range.start];
                 let during = &line[range.clone()];
                 let during = match opt.highlight {
                     true => style(during).yellow(),
-                    false => style(during),
+                    false => {
+                        let p = to_percentile(parsed);
+                        Style::from(p).apply_to(during)
+                    }
                 };
                 let during = match opt.bold {
                     true => during.bold(),
@@ -79,9 +131,12 @@ fn main() -> Result<()> {
         }
     });
 
-    println!("Found {} samples", hist.len());
-    println!("50.0'th percentile:  {}", hist.value_at_quantile(0.5));
-    println!("99.9'th percentile:  {}", hist.value_at_quantile(0.999));
+    if opt.debug {
+        println!("Number of samples: {}", hist.len());
+        println!("99'th percentile:  {}", hist.value_at_quantile(0.99));
+        println!("90'th percentile:  {}", hist.value_at_quantile(0.9));
+        println!("50'th percentile:  {}", hist.value_at_quantile(0.5));
+    }
 
     Ok(())
 }
